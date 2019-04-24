65b4d889626cf76ef3290f46def7cc036cca8ac2
James Declerus <james.declerus@esih.edu>
2019-04-24 12:41:12 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,16 @@
 # Make your shoe class here!
+
+class Shoe
+attr_accessor  :size, :material, :color, :condition, :cobble
+attr_reader :brand
+BRANDS = []
+def initialize(brand)
+  @brand = brand
+  BRANDS << brand unless BRANDS.any? { |b| b == brand }
+end
+
+def cobble
+  self.condition = "new"
+
+end
+end
